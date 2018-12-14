93f04eb688048ad3434d3a39312574d76206ff4a
daizi <daizq@getui.com>
2018-12-14 15:34:43 +0800

    update.

--- pod.sh
+++ pod.sh
@@ -13,10 +13,27 @@
 # git push
 # git tag -a $GeshuVersion -m "$GeshuVersion"
 # git push origin --tags
-git push origin --delete tag "1.4.4.0"
-v=$(pod spec lint DaizqPod.podspec)
-if [[ $v =~ "ERROR" ]]; then
-	echo "fail"
+# SDK_VERSION=$(grep GC_SDK_VSERSION  GCSDK/GTCountSDK/GTCountSDK/Main/Const/GCConst.h | sed -n "1p" | sed 's/.*\([0-9].[0-9].[0-9].[0-9]\).*/\1/g')
+# echo $SDK_VERSION
+# exit;
+SDK_VERSION="1.4.2.0"
+git tag -d $SDK_VERSION
+git push origin :refs/tags/$SDK_VERSION
+git tag -a $SDK_VERSION -m "$SDK_VERSION"
+git push origin --tags
+
+isValid=$(pod spec lint DaizqPod.podspec)
+echo $isValid
+if [[ $isValid =~ "ERROR" ]]; then
+	echo "pod valid fail";
+	exit;
 else
-	echo "succuss"
+	podPushMsg=$(pod trunk push)
+	echo $podPushMsg
+	if [[ $podPushMsg =~ "successfully published" ]]; then
+		echo "pod successfully published"
+		echo run=true >var.txt
+	else
+		echo "pod published fail"
+	fi
 fi
